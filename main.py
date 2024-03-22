11f7ff742d427513d32c75e7e8d7bf780c363dfb
arcrystal <acrystal@u.rochester.edu>
2024-03-22 14:25:26 -0400

    make main.py a test script

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from curate import Curate
 from aggregate import aggregate_and_save
 
 if __name__ == '__main__':
-    scrape_wikipedia_sites("sites.txt", "test1", highres=False)
+    scrape_wikipedia_sites("test_sites.txt", "test1", highres=False)
+    scrape_wikipedia_sites("test_sites.txt", "test2", highres=True)
     # c = Curate()
     # c.process_directories("data")
     # aggregate_and_save("data")
